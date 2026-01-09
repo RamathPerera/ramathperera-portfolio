c6642effc01035ddbd8e71324502062db279c394
RamathPerera <118724044+RamathPerera@users.noreply.github.com>
2026-01-09 14:27:34 +0530

    Updates

--- _next/static/Li5lW-tBnHefCgnauM47G/_buildManifest.js
+++ _next/static/Li5lW-tBnHefCgnauM47G/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/ramathperera-portfolio//_next/:path+",
-        "destination": "/ramathperera-portfolio/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
