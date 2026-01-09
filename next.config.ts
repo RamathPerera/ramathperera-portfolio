526fb51cabc9a442f326a304f05f4f5b00234de0
RamathPerera <118724044+RamathPerera@users.noreply.github.com>
2026-01-09 15:13:10 +0530

    Setup static export and deployment action

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   images: {
+  output: "export",
+  images: {
+    unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "**",
       },
     ],
   },
